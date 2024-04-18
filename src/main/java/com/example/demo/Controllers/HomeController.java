1bd4733ad5fc8781dead87f8faf90a9fcdbb2a5b
adamlhardy <adam.hardy37@gmail.com>
2024-04-18 15:58:56 -0700

    "feat: connected application to mongodb"

--- src/main/java/com/example/demo/Controllers/HomeController.java
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -1,15 +1,18 @@
 package com.example.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * HomeController
  */
-@Controller
-@RequestMapping("/")
+@RestController
+//@RequestMapping("/")
 public class HomeController {
 
+    @GetMapping("/")
     public String getHome() {
         return "Welcome to the home page";
     }
--- src/main/java/com/example/demo/Controllers/RestaurantController.java
+++ src/main/java/com/example/demo/Controllers/RestaurantController.java
@@ -1,11 +1,27 @@
 package com.example.demo.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.Models.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Repositories.RestaurantRepository;
 
 @RestController
 @RequestMapping("/api/restaurants")
 public class RestaurantController {
+    
+    @Autowired
+    RestaurantRepository restaurantRepository;
 
-
+    @GetMapping("/{borough}")
+    public List<Restaurant> getRestaurantByBorough(@PathVariable(value = "borough") String boroughName) {
+       var result = restaurantRepository.findRestaurantByBorough(boroughName);
+       return result;
+    }
 }
